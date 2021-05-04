6db249f1c2e9a625c6d010c213d6c8932a560d95
2010-lab <75510340+2010-lab@users.noreply.github.com>
2021-05-03 19:07:02 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var canvas = new fabric.canvas('myCanvas')
+block_image_width = 30;
+block_image_height = 30;
+player_x = 10;
+player_y = 10;
+var player_object = "";
+function player_update()
+{
+    fabric.Image.fromURL("player.png", function(Image)
+    {player_object = img;
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+            top:player_y,
+            left:player_x
+        });
+        canvas.add(player_object);
+
+    
+    }
+function new_image(get_image)
+fabric.Image.fromURL("player.png", function(Image)
+    {player_object = img;
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+            top:player_y,
+            left:player_x
+            canvas.add(block_image_object);
+        });
+
+
